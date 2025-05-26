44e9000b473643f7185f2722ef9d11a4f10bebd5
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-05-26 20:29:44 +0330

    add shadcnSelekt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from 'react'
 import { useDarkMode } from '../context/DarkModeContext'
 import Toggle from './Toggle'
 import { Moon } from 'lucide-react'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
 
 export default function Header({ onFontChange }) {
 	const [selectedFont, setSelectedFont] = useState('Inter')
@@ -17,22 +24,50 @@ export default function Header({ onFontChange }) {
 				<img src="/assets/images/logo.svg" alt="logo" />
 			</div>
 			<div className="flex items-center gap-x-12">
-				<select
+				<Select
 					value={selectedFont}
-					onChange={e => {
-						setSelectedFont(e.target.value)
-						onFontChange(e.target.value)
+					onValueChange={value => {
+						setSelectedFont(value)
+						onFontChange(value)
 					}}
-					className={`${
-						isDarkMode
-							? 'text-white bg-[#050505]'
-							: 'text-primary-600'
-					} p-2 rounded`}
 				>
-					<option value="Inter">Inter</option>
-					<option value="Lora">Lora</option>
-					<option value="Inconsolata">Inconsolata</option>
-				</select>
+					<SelectTrigger
+						className={`w-[180px] ${
+							isDarkMode
+								? 'text-white bg-[#050505]'
+								: 'text-primary-600'
+						} p-2 rounded`}
+					>
+						<SelectValue placeholder="Select Font" />
+					</SelectTrigger>
+					<SelectContent
+						className={`${
+							isDarkMode
+								? 'bg-[#1f1f1f] text-white shadow-[0_5px_30px_rgb(164,69,237)]'
+								: 'bg-white text-black'
+						}`}
+					>
+						<SelectItem
+							value="Inter"
+							className="hover:text-primary-50 cursor-pointer"
+						>
+							Inter
+						</SelectItem>
+						<SelectItem
+							value="Lora"
+							className="hover:text-primary-50 cursor-pointer"
+						>
+							Lora
+						</SelectItem>
+						<SelectItem
+							value="Inconsolata"
+							className="hover:text-primary-50 cursor-pointer"
+						>
+							Inconsolata
+						</SelectItem>
+					</SelectContent>
+				</Select>
+
 				<div className="flex items-center gap-x-2">
 					<Toggle />
 					<Moon
